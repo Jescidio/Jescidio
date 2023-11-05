3e0af5832b26f827952538e3deca34c9e7ef6eff
Jescidio <jedasemo@gmail.com>
2023-11-04 23:43:58 -0600

    Create README.cpp

---
+++ README.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+main{
+cout<<"Soy Jes, un aprendiz de programación. "
+  <<"Iré agregando cosas sobre conforme pase el tiempo, "
+  <<"porque la verdad es que no tengo ni la mínima idea de "
+  <<"quien soy como prgramador"
+  <<endl;
+  return 0;
+}
